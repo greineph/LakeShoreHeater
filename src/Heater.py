c2d6030337b4bd4b80cc7760de42fcdcd16aeac5
Philipp <philipp.greiner@uni-a.de>
2025-05-10 20:05:34 +0200

    partial implementation of create_input_setup_settings in channel subclasses

--- src/Heater.py
+++ src/Heater.py
@@ -1,4 +1,14 @@
-import Channel
+from xlsxwriter.contenttypes import overrides
+
+from Channel import Channel
+from lakeshore import Model372InputSetupSettings
+import InputData
+
 
 class Heater(Channel):
-    pass
+
+    # creates input setup settings for heater from InputData.py
+    # TODO: clarify other settings
+    @overrides
+    def create_input_setup_settings(self):
+        return Model372InputSetupSettings(auto_range=InputData.AUTO_RANGE_MODE_THERMOMETER)
--- src/Thermometer.py
+++ src/Thermometer.py
@@ -1,4 +1,16 @@
-import Channel
+from xlsxwriter.contenttypes import overrides
+
+from Channel import Channel
+from lakeshore import Model372InputSetupSettings
+import InputData
+
 
 class Thermometer(Channel):
-    pass
+
+    # creates input setup settings for thermometer from InputData.py
+    # TODO: clarify other settings
+    @overrides
+    def create_input_setup_settings(self):
+        return Model372InputSetupSettings(mode=InputData.SENSOR_EXCITATION_MODE_THERMOMETER,
+                                          auto_range=InputData.AUTO_RANGE_MODE_THERMOMETER)
+
