006d34c98b3974165f8a47fd2bb52b9f244798e3
Philipp <philipp.greiner@uni-a.de>
2025-07-06 20:47:40 +0200

    made MPVWrapper thread save

--- src/MPVWrapper.py
+++ src/MPVWrapper.py
@@ -1,5 +1,10 @@
+import traceback
+
 import MultiPyVu as mpv
+import numpy as np
+
 import InputData
+import threading
 
 
 # TODO: make thread save
@@ -8,21 +13,42 @@ class MPVWrapper:
     def __init__(self):
         self.server = mpv.Server()
         self.client = mpv.Client()
+        self.lock = threading.Lock()
 
         self.wanted_reading_keys = []
         self.wanted_reading_names = []
         self.wanted_plotting_names = []
 
-        self.key_to_function = {"temperature": self.client.get_temperature,
-                                "field": self.client.get_field}
+        self.key_to_function = {"temperature": self.get_temperature,
+                                "field": self.get_field}
 
         self.server.open()
         self.client.open()
 
+    def get_temperature(self):
+        self.lock.acquire(blocking=True)
+        value = np.nan
+        try:
+            value = self.client.get_temperature()[0]
+        except Exception:
+            print(traceback.format_exc())
+        self.lock.release()
+        return value
+
+    def get_field(self):
+        self.lock.acquire(blocking=True)
+        value = np.nan
+        try:
+            value = self.client.get_field()[0]
+        except Exception:
+            print(traceback.format_exc())
+        self.lock.release()
+        return value
+
     def get_wanted_readings(self) -> list:
         readings = []
         for key in self.get_wanted_reading_keys():
-            readings.append(self.key_to_function[key]()[0])
+            readings.append(self.key_to_function[key]())
         return readings
 
     # gets the selected readings from InputData
@@ -33,7 +59,7 @@ class MPVWrapper:
     def get_readings(self):
         readings = {}
         for key in self.key_to_function:
-            readings[key] = self.key_to_function[key]()[0]
+            readings[key] = self.key_to_function[key]()
         return readings
 
     # handles shutting down the properties in this wrapper
