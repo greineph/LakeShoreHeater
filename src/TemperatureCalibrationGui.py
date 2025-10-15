05435d0d2afdeb9a68df46929b0fabc79bcf8833
Philipp Greiner <p.greiner.mm@gmail.com>
2025-10-15 13:57:46 +0200

    added indicator for incomplete and false inputs in TemperatureCalibrationGui.py

--- src/TemperatureCalibrationGui.py
+++ src/TemperatureCalibrationGui.py
@@ -40,9 +40,13 @@ class TemperatureCalibrationGui(qtw.QWidget):
         form_holder.setLayout(form_layout)
         self.layout().addWidget(form_holder)
 
-        name = qtw.QLineEdit()
+        name = qtw.QLineEdit(self)
         form_layout.addRow("Name", name)
         self.form["name"] = name
+        # name.setStyleSheet("border-style: solid; border-width: 2px; border-color: red;")
+        print(name.styleSheet())
+        print(self.styleSheet())
+        print(self.style())
 
         min_resistance = qtw.QLineEdit()
         form_layout.addRow("Minimum Resistance", min_resistance)
@@ -83,26 +87,84 @@ class TemperatureCalibrationGui(qtw.QWidget):
         self.show()
 
     def create_file(self):
+        has_error = False
+        error_style_sheet = "border-style: solid; border-width: 2px; border-color: red; font-size: 16pt;"
+        default_style_sheet = "font-size: 16pt;"
+
         name = GuiHelper.get_data_from_widget(self.form["name"])
+        if len(name) == 0:
+            self.form["name"].setStyleSheet(error_style_sheet)
+            has_error = True
+        else:
+            self.form["name"].setStyleSheet(default_style_sheet)
+
         min_resistance = GuiHelper.get_data_from_widget(self.form["min_resistance"])
         print(min_resistance)
-        min_resistance = float(min_resistance)
+        if not is_number(min_resistance):
+            self.form["min_resistance"].setStyleSheet(error_style_sheet)
+            has_error = True
+        else:
+            min_resistance = float(min_resistance)
+            self.form["min_resistance"].setStyleSheet(default_style_sheet)
+
         max_resistance = GuiHelper.get_data_from_widget(self.form["max_resistance"])
         print(max_resistance)
-        max_resistance = float(max_resistance)
+        if not is_number(max_resistance):
+            self.form["max_resistance"].setStyleSheet(error_style_sheet)
+            has_error = True
+        else:
+            max_resistance = float(max_resistance)
+            self.form["max_resistance"].setStyleSheet(default_style_sheet)
+
         rescale_0 = GuiHelper.get_data_from_widget(self.form["rescale_0"])
         print(rescale_0)
-        rescale_0 = float(rescale_0)
+        if not is_number(rescale_0):
+            self.form["rescale_0"].setStyleSheet(error_style_sheet)
+            has_error = True
+        else:
+            rescale_0 = float(rescale_0)
+            self.form["rescale_0"].setStyleSheet(default_style_sheet)
+
         rescale_1 = GuiHelper.get_data_from_widget(self.form["rescale_1"])
         print(rescale_1)
-        rescale_1 = float(rescale_1)
+        if not is_number(rescale_1):
+            self.form["rescale_1"].setStyleSheet(error_style_sheet)
+            has_error = True
+        else:
+            rescale_1 = float(rescale_1)
+            self.form["rescale_1"].setStyleSheet(default_style_sheet)
+
         func_params = GuiHelper.get_data_from_widget(self.form["func_params"])
+        func_params = func_params.split("\n")
         print(func_params)
-        func_params = [float(l) for l in func_params.split("\n")]
+        self.form["func_params"].setStyleSheet(default_style_sheet)
+        for n in func_params:
+            if not is_number(n):
+                self.form["func_params"].setStyleSheet(error_style_sheet)
+                has_error = True
+                break
+        if not has_error:
+            func_params = [float(n) for n in func_params]
+
         min_temperature = GuiHelper.get_data_from_widget(self.form["min_temperature"])
-        min_temperature = float(min_temperature)
+        if not is_number(min_temperature):
+            self.form["min_temperature"].setStyleSheet(error_style_sheet)
+            has_error = True
+        else:
+            min_temperature = float(min_temperature)
+            self.form["min_temperature"].setStyleSheet(default_style_sheet)
+
         max_temperature = GuiHelper.get_data_from_widget(self.form["max_temperature"])
-        max_temperature = float(max_temperature)
+        if not is_number(max_temperature):
+            self.form["max_temperature"].setStyleSheet(error_style_sheet)
+            has_error = True
+        else:
+            max_temperature = float(max_temperature)
+            self.form["max_temperature"].setStyleSheet(default_style_sheet)
+
+        if has_error:
+            print("inputs were incomplete or wrong")
+            return
 
         values = {
             "name": name,
@@ -123,6 +185,14 @@ class TemperatureCalibrationGui(qtw.QWidget):
         with open(os.path.join(path, str(name) + ".json"), "w") as file:
             s = json.dumps(values, indent=4)
             file.write(s)
+
+
+def is_number(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
 
 
 # just for testing ui
